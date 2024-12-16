83bde08b98998a8db43de83ba61d29597d780a67
Daniel Niknam <daniel-niknam@users.noreply.github.com>
2024-12-16 22:57:27 +1100

    Update lazy

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -89,17 +89,6 @@ return {
 				-- configure emmet language server
 				lspconfig["emmet_ls"].setup({
 					capabilities = capabilities,
-					filetypes = {
-						"erb",
-						"html",
-						"typescriptreact",
-						"javascriptreact",
-						"css",
-						"sass",
-						"scss",
-						"less",
-						"svelte",
-					},
 				})
 			end,
 			["lua_ls"] = function()
